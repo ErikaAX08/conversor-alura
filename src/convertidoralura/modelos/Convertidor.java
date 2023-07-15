8e2a7c204049fe9111fe291f7d75b0af0667c52e
Erika AX <eamastalxochimitl@gmail.com>
2023-07-14 22:17:40 -0600

    Modelo del convertidor

---
+++ src/convertidoralura/modelos/Convertidor.java
@@ -0,0 +1,19 @@
+package convertidoralura.modelos;
+
+/**
+ *
+ * @author Erika AX
+ */
+public class Convertidor {
+    
+    private Conversor conversor;
+    
+    public void setConversor(Conversor conversor) {
+        this.conversor = conversor;
+    }
+    
+    public Conversor getConversor(){
+        return conversor;
+    }
+
+}
